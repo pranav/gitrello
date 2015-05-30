1c38730314fbb2b5aa134e216cb1040876c2a5f4
Pranav Gandhi <pgandhi@hubspot.com>
2015-05-30 18:46:32 -0400

    Add GUICE

--- src/main/java/io/pranav/gitrello/GitrelloApplication.java
+++ src/main/java/io/pranav/gitrello/GitrelloApplication.java
@@ -2,6 +2,7 @@ package io.pranav.gitrello;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -10,22 +11,31 @@ import io.pranav.gitrello.resources.GithubIssueResource;
 
 public class GitrelloApplication extends Application<GitrelloConfiguration>{
 
+  private GuiceBundle<GitrelloConfiguration> guiceBundle;
+
   public static void main(String[] args) throws Exception {
     new GitrelloApplication().run(args);
   }
 
   @Override
-  public void initialize(Bootstrap<GitrelloConfiguration> bootstrap) {
+  public String getName() {
+    return "gitrello";
+  }
 
+  @Override
+  public void initialize(Bootstrap<GitrelloConfiguration> bootstrap) {
+    guiceBundle = GuiceBundle.<GitrelloConfiguration>newBuilder()
+        .addModule(new GitrelloModule())
+        .enableAutoConfig(getClass().getPackage().getName())
+        .setConfigClass(GitrelloConfiguration.class)
+        .build();
+
+    bootstrap.addBundle(guiceBundle);
   }
 
   @Override
   public void run(GitrelloConfiguration configuration, Environment environment) {
     environment.getObjectMapper().setPropertyNamingStrategy(
       PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-
-    final GithubIssueResource githubIssueResource = new GithubIssueResource();
-    environment.jersey().register(githubIssueResource);
-
   }
 }
--- src/main/java/io/pranav/gitrello/GitrelloConfiguration.java
+++ src/main/java/io/pranav/gitrello/GitrelloConfiguration.java
@@ -1,7 +1,31 @@
 package io.pranav.gitrello;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class GitrelloConfiguration extends Configuration {
 
+  @NotEmpty public String trelloApiKey;
+  @NotEmpty public String trelloApiSecret;
+
+  @JsonProperty
+  public String getTrelloApiKey() {
+    return trelloApiKey;
+  }
+
+  @JsonProperty
+  public void setTrelloApiKey(String trelloApiKey) {
+    this.trelloApiKey = trelloApiKey;
+  }
+
+  @JsonProperty
+  public String getTrelloApiSecret() {
+    return trelloApiSecret;
+  }
+
+  @JsonProperty
+  public void setTrelloApiSecret(String trelloApiSecret) {
+    this.trelloApiSecret = trelloApiSecret;
+  }
 }
